7572819a8877618298dea9dd99fc0730b2e87584
Neo <DavidEddlemon@Gmail.com>
2017-03-14 10:47:38 -0500

    There is no spoon...
    shamu: Stop logcat spam due to touchboost and mp-decision
    Thanks goes out to krexus-partners team and all others involved
    
    Speakerphone In-Call Echo Fix
    Thanks goes out to Yoinx and the rest of the DU team
    
    Boost that audio output! In-Call, Speakerphone, headphones, etc
    
    Do not force encryption
    
    Revert "Disable VT on shamu"
    
    Remove the unnecessary decreased sound delays from notifications
    
    Smooth it out a bit
    
    A few more VoLTE changes
    
    Some Nougat bringup
    
    Add notification blink support to liblights
    Thanks goes out to Griffin Millender & Christian Oder for the changes
    
    Change-Id: Ia9cd485bbf9f8612425cd8095fc55b18c01663cd
    Verizon: enable VVM3 visual voicemail

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -128,7 +128,8 @@ USE_DEVICE_SPECIFIC_CAMERA:= true
 
 BOARD_HAL_STATIC_LIBRARIES := libdumpstate.shamu
 
-USE_CLANG_PLATFORM_BUILD := true
+# Use Snapdragon LLVM for Nightlies, if available
+TARGET_USE_SDCLANG := true
 
 # Disable dex-preopt of prebuilts to save space.
 DONT_DEXPREOPT_PREBUILTS := true
